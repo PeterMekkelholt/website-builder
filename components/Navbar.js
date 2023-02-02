f4e52f0c2062506d511ee8297127fa026bfc337f
You_Found_Josh <zingomarket@gmail.com>
2023-02-02 12:53:29 +0000

    Test

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,30 +1,25 @@
-import Link from "next/link";
-import Image from "next/image";
-import React, { useState } from "react";
+import Link from 'next/link'
+import Image from 'next/image'
+import React, { useState } from 'react'
 import logo from '../public/sample-logo.png'
-import NavItem from "./NavItem";
+import NavItem from './NavItem'
 
 const MENU_LIST = [
-  { text: "Products", href: "/products" },
-  { text: "About Us", href: "/about" },
-  { text: "Contact", href: "/contact" },
-];
+  { text: 'Products', href: '/products' },
+  { text: 'About Us 2', href: '/about' },
+  { text: 'Contact', href: '/contact' },
+]
 const Navbar = () => {
-  const [navActive, setNavActive] = useState(null);
-  const [activeIdx, setActiveIdx] = useState(-1);
+  const [navActive, setNavActive] = useState(null)
+  const [activeIdx, setActiveIdx] = useState(-1)
 
   return (
     <header>
       <nav className={`nav`}>
-          <Link href={"/"}>
-            <Image
-              src={logo}
-              alt="Sample logo"
-              width={100}
-              height={100}
-              />
-          </Link>
-        
+        <Link href={'/'}>
+          <Image src={logo} alt="Sample logo" width={100} height={100} />
+        </Link>
+
         <div
           onClick={() => setNavActive(!navActive)}
           className={`nav__menu-bar`}
@@ -33,12 +28,12 @@ const Navbar = () => {
           <div></div>
           <div></div>
         </div>
-        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+        <div className={`${navActive ? 'active' : ''} nav__menu-list`}>
           {MENU_LIST.map((menu, idx) => (
             <div
               onClick={() => {
-                setActiveIdx(idx);
-                setNavActive(false);
+                setActiveIdx(idx)
+                setNavActive(false)
               }}
               key={menu.text}
             >
@@ -48,7 +43,7 @@ const Navbar = () => {
         </div>
       </nav>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
